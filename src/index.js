a3d3a1bc075baa41ab693dcc35be933f062818cc
zeb <345872674@qq.com>
2018-05-07 15:03:51 +0800

    删除

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import axios from 'axios'
 import registerServiceWorker from './registerServiceWorker';
 
-
+console.log(process.env)
 axios.get(`/feed/1.0.0/update.json`).then(res => {
   console.log(res)
 });
