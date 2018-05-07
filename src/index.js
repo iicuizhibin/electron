78acbf18e8397d73da9d865a6033e2c10f39bc91
zeb <345872674@qq.com>
2018-05-07 14:23:58 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios'
 import registerServiceWorker from './registerServiceWorker';
 
+
+axios.get(`/feed/1.0.0/update.json`).then(res => {
+  console.log(res)
+});
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
